293cbe068121d6259dd36e44c3ac442a46696bdd
colopisalvatore <salvatorecolopi1989@gmail.com>
2023-02-09 16:39:58 +0100

    add style to social icon

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -128,4 +128,110 @@ ul {
     }
 
 }
+
+// SOCIAL ICON
+.wrapper {
+    .icon {
+        position: relative;
+        background-color: #ffffff;
+        border-radius: 50%;
+        margin: 10px;
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        font-size: 22px;
+        display: inline-block;
+        align-items: center;
+        box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+        cursor: pointer;
+        transition: all 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+        color: #333;
+        text-decoration: none;
+
+        &:hover {
+
+            span,
+            .tooltip {
+                text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
+            }
+
+            .tooltip {
+                top: -45px;
+                opacity: 1;
+                visibility: visible;
+                pointer-events: auto;
+            }
+        }
+
+        &:hover,
+        &:hover .tooltip,
+        &:hover .tooltip::before {
+            background-color: #ffffff;
+            color: #333;
+        }
+
+        &.facebook:hover,
+        &.facebook:hover .tooltip,
+        &.facebook:hover .tooltip::before {
+            background-color: #3b5999;
+            color: #ffffff;
+        }
+
+        &.twitter:hover,
+        &.twitter:hover .tooltip,
+        &.twitter:hover .tooltip::before {
+            background-color: #46c1f6;
+            color: #ffffff;
+        }
+
+        &.instagram:hover,
+        &.instagram:hover .tooltip,
+        &.instagram:hover .tooltip::before {
+            background-color: #e1306c;
+            color: #ffffff;
+        }
+
+        &.github:hover,
+        &.github:hover .tooltip,
+        &.github:hover .tooltip::before {
+            background-color: #333333;
+            color: #ffffff;
+        }
+
+        &.youtube:hover,
+        &.youtube:hover .tooltip,
+        &.youtube:hover .tooltip::before {
+            background-color: #de463b;
+            color: #ffffff;
+        }
+    }
+
+
+    .tooltip {
+        position: absolute;
+        top: 0;
+        line-height: 1.5;
+        font-size: 14px;
+        background-color: #ffffff;
+        color: #ffffff;
+        padding: 5px 8px;
+        border-radius: 5px;
+        box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+        opacity: 0;
+        pointer-events: none;
+        transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+
+        &::before {
+            position: absolute;
+            content: "";
+            height: 8px;
+            width: 8px;
+            background-color: #ffffff;
+            bottom: -3px;
+            left: 50%;
+            transform: translate(-50%) rotate(45deg);
+            transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+        }
+    }
+}
 </style>
