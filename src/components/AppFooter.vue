f3f4597fcfadfab54cfd17fc9d592b11dcf93961
colopisalvatore <salvatorecolopi1989@gmail.com>
2023-02-09 16:45:14 +0100

    optimize scss code

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -89,36 +89,32 @@ ul {
     }
 
     :hover {
-        color: white;
+        color: grey;
     }
 
 
 }
 
 .hover-underline-animation {
-
     position: relative;
 
+    &:after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        transform: scaleX(0);
+        height: 2px;
+        bottom: 0;
+        left: 0;
+        background-color: $red;
+        transform-origin: bottom right;
+        transition: transform 0.25s ease-out;
+    }
 
-
-}
-
-.hover-underline-animation:after {
-    content: '';
-    position: absolute;
-    width: 100%;
-    transform: scaleX(0);
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: $red;
-    transform-origin: bottom right;
-    transition: transform 0.25s ease-out;
-}
-
-.hover-underline-animation:hover:after {
-    transform: scaleX(1);
-    transform-origin: bottom left;
+    &:hover:after {
+        transform: scaleX(1);
+        transform-origin: bottom left;
+    }
 }
 
 @media only screen and (max-width: 570px) {
