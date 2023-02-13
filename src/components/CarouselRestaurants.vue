ca76216e162800d683330c2807aae395e8189d80
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2023-02-13 10:28:13 +0100

    carta carousel e carousel √

--- src/components/CarouselRestaurants.vue
+++ src/components/CarouselRestaurants.vue
@@ -12,8 +12,8 @@
                         <div class="img mb-2">
                             <img class="w-100 h-100" :src="`${store.imagBasePath}${slide.image}`" alt="">
                         </div>
-                        <h3 class="">{{ slide.name }}</h3>
-                        <p class="info">{{ truncate(slide.description, 100) }}</p>
+                        <h3 class="w-50 m-auto">{{ slide.name }}</h3>
+                        <p class="info w-50 m-auto">{{ truncate(slide.description, 120) }}</p>
 
                         <router-link :to="{ name: 'restaurant', params: { slug: slide.slug } }">
                             <button class="">
@@ -110,8 +110,8 @@ export default {
 @use './../assets/styles/partials/variables' as *;
 
 .carta {
-    width: 20em;
-    height: 22em;
+    width: 30em;
+    height: 100%;
 
     transition: 1s ease-in-out;
     clip-path: polygon(30px 0%, 100% 0, 100% calc(100% - 30px), calc(100% - 30px) 100%, 0 100%, 0% 30px);
@@ -120,11 +120,17 @@ export default {
     background-size: contain;
     background-position: center;
     background-repeat: no-repeat;
+    transition: 1s ease-in-out;;
     background-image: url("data:image/svg+xml;utf8,%3Csvg xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22 width=%22753.595%22 height=%22644.116%22 viewBox=%22112.635 218.87 753.595 644.116%22%3E%3Cdefs%3E%3CradialGradient id=%22c%22 r=%22100%25%22 cx=%222%25%22 cy=%2280%25%22%3E%3Cstop offset=%220%25%22 stop-color=%22%23FF0037%22%2F%3E%3Cstop offset=%2250%25%22 stop-color=%22%23F4F1BB%22%2F%3E%3Cstop offset=%22100%25%22 stop-color=%22%239BC1BC%22%2F%3E%3C%2FradialGradient%3E%3CclipPath id=%22b%22%3E%3Cpath fill=%22currentColor%22 d=%22M825.5 631Q762 762 631 824.5T345.5 848q-154.5-39-211-193.5t33-276q89.5-121.5 211-111T659 230q159-48 194.5 111t-28 290Z%22%2F%3E%3C%2FclipPath%3E%3Cfilter id=%22a%22 x=%22-50vw%22 y=%22-50vh%22 width=%22100vw%22 height=%22100vh%22%3E%3CfeFlood flood-color=%22%23fff%22 result=%22neutral-gray%22%2F%3E%3CfeTurbulence type=%22fractalNoise%22 baseFrequency=%222.5%22 numOctaves=%22100%22 stitchTiles=%22stitch%22 result=%22noise%22%2F%3E%3CfeColorMatrix in=%22noise%22 type=%22saturate%22 values=%220%22 result=%22destaturatedNoise%22%2F%3E%3CfeComponentTransfer in=%22desaturatedNoise%22 result=%22theNoise%22%3E%3CfeFuncA type=%22table%22 tableValues=%220 0 0.1 0%22%2F%3E%3C%2FfeComponentTransfer%3E%3CfeBlend in=%22SourceGraphic%22 in2=%22theNoise%22 mode=%22soft-light%22 result=%22noisy-image%22%2F%3E%3C%2Ffilter%3E%3C%2Fdefs%3E%3Cg filter=%22url(%23a)%22 clip-path=%22url(%23b)%22%3E%3Cpath fill=%22url(%23c)%22 d=%22M825.5 631Q762 762 631 824.5T345.5 848q-154.5-39-211-193.5t33-276q89.5-121.5 211-111T659 230q159-48 194.5 111t-28 290Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E");
 
-}
+    &:hover{
+        transform: scale(1.02);
+        .img{
+            transform: translateY(-20px);
+        }
+    }
 
-.carta span {
+    span {
     font-weight: bold;
     color: white;
     text-align: center;
@@ -133,7 +139,7 @@ export default {
     font-size: 1em;
 }
 
-.carta .info {
+.info {
     font-weight: 400;
     color: $black;
     display: block;
@@ -144,47 +150,58 @@ export default {
     margin-top: 1em;
 }
 
-.carta .img {
+
+.img {
     overflow: hidden;
-    width: 5rem;
-    height: 5rem;
+    width: 10rem;
+    height: 10rem;
     background: white;
     border-radius: 15px;
     margin: auto;
     margin-top: 1.7em;
+    transition: 2s ease-in-out;
 }
 
-.carta .share {
-    text-align: center;
-    margin-top: 1.8em;
-}
 
-.carta a {
-    color: white;
-    transition: .4s ease-in-out;
+a {
+    
     text-decoration: none;
 }
 
 
-
-.carta button {
-    padding: 0.8em 1.7em;
+button {
+    padding: 0.5em 1.7em;
     display: block;
     margin: auto;
     border-radius: 25px;
     border: none;
     margin-top: 2em;
+    margin-bottom: 2em;
     font-weight: bold;
     background: $acqua;
     color: rgb(0, 0, 0);
     transition: .4s ease-in-out;
-}
 
-.carta button:hover {
-    background: $yellow;
+
+    &:hover{
+        background: $yellow;
     color:$red;
     text-decoration: none;
+    }
 }
+
+
+}
+
+
+
+
+
+
+
+
+
+
 
 .carousel__slide--next {
     opacity: 0.5;
