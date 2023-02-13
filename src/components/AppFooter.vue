a3259294c0a0daf3c068eb1141ab72d88e29a751
colopisalvatore <salvatorecolopi1989@gmail.com>
2023-02-13 16:11:42 +0100

    reworked footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,54 +1,59 @@
 <template>
-
-
-    <div class="container ">
-        <div class="d-flex  ">
-            <div class="flex-column align-items-center">
-                <div class="col-12 text-center">
-                    <img class="img pt-3" src="../../public/img/logo-01.png" alt="">
-                </div>
-                <!-- Copyright -->
-                <div class="mt-1 text-center">
-                    2023 <span class="text-reset fw-bold">Codeat <sup>®</sup></span>
-                </div>
-                <!-- Copyright -->
-
+<div class="container">
+    <div class="d-flex flex-wrap">
+        <div class="d-flex flex-column align-items-center col-12 col-md-4">
+            <img class="img pt-2" src="../../public/img/logo-01.png" alt="">
+            <div class="mt-1">
+                2023 <span class="text-reset fw-bold">Codeat <sup>®</sup></span>
             </div>
-            <div class="d-flex flex-grow-1 justify-content-around align-items-baseline ">
-
-                <div v-for="(item, index) in store.footerItems" :key="index">
-                    <ul class="">{{ item.label }}<span class=" dot">.</span>
-                        <li v-for="x in item.storeItem" class="hover-underline-animation">
-                            {{ x.pagename }}
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="wrapper d-flex flex-wrap flex-column  ">
-                    <div class="">
-                        <h3 class="m-0 ">Social <span class="dot">.</span></h3>
-                    </div>
-
-                    <div class="mt-3">
-                        <div class="d-flex flex-wrap">
-
-                            <a v-for="link in socialLinks" :key="link.name" :href="link.url" class="icon"
-                            :class="link.name.toLowerCase()">
-                            <div class="tooltip">{{ link.name }}</div>
-                            <span><i :class="`fa-brands ${link.icon}`"></i></span>
-
-                        </a>
+        </div>
+        <div class="d-flex flex-column flex-sm-row justify-content-around align-items-baseline col-12 col-sm-12 col-md-8">
+            <!-- DISPLAY UP 768px -->
+            <div v-for="(item, index) in store.footerItems" :key="index" class="col-14 col-sm-4 d-none d-md-block">
+                <ul>
+                    <h3>{{ item.label }}<span class="dot">.</span></h3>
+                    <li v-for="x in item.storeItem" class="hover-underline-animation">
+                        <template v-if="x.icon">
+                            <i :class="'fa-brands' + ' ' + x.icon"></i>
+                        </template>
+                        {{ x.pagename }}
+                    </li>
+                </ul>
+            </div>
+    
+            <!-- DISPLAY UNDER 768px -->
+            <div class="container p-5 d-md-none">
+                <div class="accordion" id="accordionExample">
+                    <template v-if="windowWidth >= 768">
+                    </template>
+                    <div class="accordion-item" v-for="(item, index) in store.footerItems" :key="item">
+                        <h2 class="accordion-header" :id="'heading' + item">
+                            <button class="accordion-button" :class="{ 'collapsed': index !== activeIndex }" type="button" @click="toggle(index)"
+                                :aria-expanded="index === activeIndex" :aria-controls="'collapse-'+item.label">
+                                {{item.label}}
+                            </button>
+                        </h2>
+                        <div :id="'collapse-' + item.label" class="accordion-collapse collapse"
+                            :class="{ 'show': index === activeIndex }" 
+                            :aria-labelledby="'heading' + item"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <ul>
+                                    <li v-for="x in item.storeItem" class="hover-underline-animation">
+                                        <template v-if="x.icon">
+                                            <i :class="'fa-brands' + ' ' + x.icon"></i>
+                                        </template>
+                                        {{ x.pagename }}
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
-
-
+</div>
 </template>
 
 <script>
@@ -58,14 +63,15 @@ export default {
 
     data() {
         return {
-            store
+            store,
+            activeIndex: 0,
         }
     },
-    computed: {
-        socialLinks() {
-            return store.socialLinks;
-        }
-    }
+    methods: {
+        toggle(index) {
+            this.activeIndex = this.activeIndex === index ? -1 : index;
+        },
+    }    
 }
 </script>
 
@@ -81,6 +87,9 @@ export default {
 h3 {
     font-size: 22px;
     font-weight: 500;
+    &:hover {
+            color: #000!important;
+        }
 }
 
 img {
@@ -109,7 +118,6 @@ ul {
 
 
 }
-
 .hover-underline-animation {
     position: relative;
 
@@ -131,149 +139,23 @@ ul {
         transform-origin: bottom left;
     }
 }
+@media only screen and (max-width: 768px) {
+   .img{
+    align-items: center!important;
+   }
+   
+   .accordion-button {
+       transition: all 0.3s ease-in-out;
 
-@media only screen and (max-width: 570px) {
+       &:not(.collapsed) {
+           color: #fff;
+           background: linear-gradient(to right, #ED6A5A, #F4F1BB, #9BC1BC);
+       }
 
-    ul {
-        display: none;
-    }
-
-    .wrapper {
-
-        h3,
-        a,
-        div,
-        span {
-
-            display: none;
-        }
-    }
-
-}
-
-// SOCIAL ICON
-.wrapper {
-    .icon {
-        position: relative;
-        background-color: #ffffff;
-        border-radius: 50%;
-        margin: 0.2em;
-        width: 2em;
-        height: 2em;
-
-        font-size: 1.5em;
-        display: inline-block;
-        align-items: center;
-        box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
-        cursor: pointer;
-        transition: all 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
-        color: #333;
-        text-decoration: none;
-
-
-        span {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            font-size:1em;
-            font-weight: 500;
-
-            text-align: center;
-            color: #333;
-            display: inline-block;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-        }
-
-        &:hover {
-
-            span {
-                color: white;
-            }
-
-            .tooltip {
-                text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
-            }
-
-            .tooltip {
-                top: -80%;
-                opacity: 1;
-                visibility: visible;
-                pointer-events: auto;
-            }
-        }
-
-        &:hover,
-        &:hover .tooltip,
-        &:hover .tooltip::before {
-            background-color: #ffffff;
-            color: #333;
-        }
-
-        &.facebook:hover,
-        &.facebook:hover .tooltip,
-        &.facebook:hover .tooltip::before {
-            background-color: #3b5999;
-            color: #ffffff;
-        }
-
-        &.twitter:hover,
-        &.twitter:hover .tooltip,
-        &.twitter:hover .tooltip::before {
-            background-color: #46c1f6;
-            color: #ffffff;
-        }
-
-        &.instagram:hover,
-        &.instagram:hover .tooltip,
-        &.instagram:hover .tooltip::before {
-            background-color: #e1306c;
-            color: #ffffff;
-        }
-
-        &.github:hover,
-        &.github:hover .tooltip,
-        &.github:hover .tooltip::before {
-            background-color: #333333;
-            color: #ffffff;
-        }
-
-        &.youtube:hover,
-        &.youtube:hover .tooltip,
-        &.youtube:hover .tooltip::before {
-            background-color: #de463b;
-            color: #ffffff;
-        }
-    }
-
-
-    .tooltip {
-        position: absolute;
-        top: 0;
-        line-height: 1.5;
-        font-size: 14px;
-        background-color: #ffffff;
-        color: #ffffff;
-        padding: 5px 8px;
-        border-radius: 5px;
-        box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
-        opacity: 0;
-        pointer-events: none;
-        transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
-
-        &::before {
-            position: absolute;
-            content: "";
-            height: 8px;
-            width: 8px;
-            background-color: #ffffff;
-            bottom: -3px;
-            left: 50%;
-            transform: translate(-50%) rotate(45deg);
-            transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
-        }
-    }
+       &:focus {
+           border-color: #fff !important;
+           box-shadow: none !important;
+       }
+   }
 }
 </style>
