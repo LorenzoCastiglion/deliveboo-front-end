c68138140e705b72a25c6f8946b46a55208ab28d
MicheleBenaco <benaco.michele@yahoo.it>
2023-02-14 21:38:55 +0100

    carosel

---
+++ src/components/TextAnimationAbout.vue
@@ -0,0 +1,117 @@
+<template>
+    <h3 ref="targetEl" id="bubble-hover" class="text-center">
+        
+      <span v-for="(char, idx) in phrase" :key="idx" :data-index="idx" class="hover-char" @mouseover="mouseOverHandler" @mouseleave="removeClasses" >{{ char }}</span>
+    </h3>
+  </template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+    name: 'TeamAnimationAbout',
+    setup() {
+        const phrase = ref("Chi Siamo?")
+        const targetEl = ref(null)
+        const hoverChars = ref(null)
+
+        onMounted(() => {
+  hoverChars.value = document.querySelectorAll('.hover-char')
+})
+
+        const removeClasses = () => {
+            hoverChars.value.forEach((char) => {
+                char.classList.remove("hovered")
+                char.classList.remove("hovered-adjacent")
+                char.classList.remove("hovered-adjacent-plus")
+            })
+        }
+
+        const mouseOverHandler = (e) => {
+            removeClasses()
+            const currentElement = e.target
+            const index = parseInt(currentElement.getAttribute("data-index"), 10)
+            const prevIndex = index === 0 ? null : index - 1
+            const nextIndex = index === phrase.value.length - 1 ? null : index + 1
+            const prevPrevIndex = index === 0 ? null : index - 2
+            const nextNextIndex = index === phrase.value.length - 1 ? null : index + 2
+            const prevEl = prevIndex !== null && document.querySelector(`[data-index="${prevIndex}"]`)
+            const nextEl = prevIndex !== null && document.querySelector(`[data-index="${nextIndex}"]`)
+            const prevPrevEl = prevIndex !== null && document.querySelector(`[data-index="${prevPrevIndex}"]`)
+            const nextNextEl = prevIndex !== null && document.querySelector(`[data-index="${nextNextIndex}"]`)
+            e.target.classList.add("hovered")
+            prevEl && prevEl.classList.add("hovered-adjacent")
+            nextEl && nextEl.classList.add("hovered-adjacent")
+            prevPrevEl && prevPrevEl.classList.add("hovered-adjacent-plus")
+            nextNextEl && nextNextEl.classList.add("hovered-adjacent-plus")
+        }
+
+        return {
+            phrase,
+            mouseOverHandler,
+            removeClasses
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@use './../assets/styles/partials/variables' as *;
+
+
+* {
+    font-family: 'Mona Sans', sans-serif;
+    // transition: all;
+    transition:0.15s ease-in-out;
+    font-feature-settings: unset;
+}
+
+body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 300px;
+}
+
+h3 {
+    
+    font-size: 100px;
+    color:$black;
+    font-weight: bold;
+    cursor: default;
+    letter-spacing: 0.03em;
+   
+    
+}
+
+
+.hovered {
+    color:$acqua;
+    font-weight: 900;
+    letter-spacing: 0.1em;
+    
+}
+
+.hovered-adjacent {
+    color: $acqua;
+    font-weight: 600;
+    letter-spacing: 0.09em;
+   
+}
+.hovered-adjacent-plus{
+   
+    color: $acqua;
+    font-weight: 300;
+    
+    
+
+}
+
+@media (max-width: 768px){
+    h3{
+        font-size: 50px;
+    }
+}
+
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,52 +1,95 @@
 <template>
+    <section class="about-container d-flex justify-content-center align-items-center flex-wrap pt-5">
+        <div class="position-absolute blobtainer col-12 ">
+            <div class="blob "></div>
+        </div>
+        <!-- SCRITTA CENTRALE -->
+        <div class="container col-12">
+            <TextAnimationAbout></TextAnimationAbout>
+        </div>
 
-    test di cose
-    <div class="container-test overflow-auto">
-        <FadeIntoView>
-            <div class="demo">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque fuga quod, temporibus aliquam aliquid exercitationem quis distinctio voluptatem quisquam ipsa expedita delectus sint excepturi itaque necessitatibus molestias! Repudiandae dignissimos qui illo et accusamus, quo magni repellat error incidunt voluptates provident sunt nisi ex facere totam temporibus expedita sapiente cum numquam dolorem. Nesciunt esse, reprehenderit nulla explicabo provident a blanditiis, quaerat fugiat maiores quidem ab tempora corrupti quas, officiis distinctio non. Animi, veniam voluptatem. Pariatur repellendus fugit, veritatis at maiores aut consectetur sequi quis. Sed quasi laboriosam, vel voluptatum delectus a, nihil dolore earum, provident iure eius distinctio doloremque dolores consectetur.
+        <div class="container">
+            <h2 class="">
+                Siamo un team di sviluppatori web junior con le  competenze di base nella progettazione e sviluppo di siti web. 
+                
+            </h2>
+
+            <h2 class="pt-2 pb-2">
+                I membri del team possono  progettare l'interfaccia utente e la grafica, sviluppare codici HTML, CSS e JavaScript per progettare front-end, scrivere codice back-end in linguaggi come PHP, 
+                impostare database, integrare API e creare strumenti di amministrazione.
+            </h2>
+
+            <h2 class="pb-5">
+                Il nostro team di sviluppatori web è riuscito a rendere il sito Web moderno, responsive e sicuro. 
+                Utilizzando le tecnologie più recenti, come l'utilizzo di framework come Bootstrap, Vue.
+            </h2>
+
+
         </div>
-        </FadeIntoView>
-        <FadeIntoView>
-            <div class="demo">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque fuga quod, temporibus aliquam aliquid exercitationem quis distinctio voluptatem quisquam ipsa expedita delectus sint excepturi itaque necessitatibus molestias! Repudiandae dignissimos qui illo et accusamus, quo magni repellat error incidunt voluptates provident sunt nisi ex facere totam temporibus expedita sapiente cum numquam dolorem. Nesciunt esse, reprehenderit nulla explicabo provident a blanditiis, quaerat fugiat maiores quidem ab tempora corrupti quas, officiis distinctio non. Animi, veniam voluptatem. Pariatur repellendus fugit, veritatis at maiores aut consectetur sequi quis. Sed quasi laboriosam, vel voluptatum delectus a, nihil dolore earum, provident iure eius distinctio doloremque dolores consectetur.
-        </div>
-        </FadeIntoView>
-        <FadeIntoView>
-            <div class="demo">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque fuga quod, temporibus aliquam aliquid exercitationem quis distinctio voluptatem quisquam ipsa expedita delectus sint excepturi itaque necessitatibus molestias! Repudiandae dignissimos qui illo et accusamus, quo magni repellat error incidunt voluptates provident sunt nisi ex facere totam temporibus expedita sapiente cum numquam dolorem. Nesciunt esse, reprehenderit nulla explicabo provident a blanditiis, quaerat fugiat maiores quidem ab tempora corrupti quas, officiis distinctio non. Animi, veniam voluptatem. Pariatur repellendus fugit, veritatis at maiores aut consectetur sequi quis. Sed quasi laboriosam, vel voluptatum delectus a, nihil dolore earum, provident iure eius distinctio doloremque dolores consectetur.
-        </div>
-        </FadeIntoView>
-    </div>
+        <!--FINE SCRITTA CENTRALE --> 
+
+        <!-- CAROSELLO TRAM :) -->
+            <div class=" container pt-5 pb-5">
+                
+                
+            <CarouselAboutTeam></CarouselAboutTeam>
+            
+            
+            
+            </div>
+
+
+
+    </section>
+
 
 </template>
 
 <script>
-import FadeIntoView from '../components/FadeIntoView.vue';
+import TextAnimationAbout from '../components/TextAnimationAbout.vue';
+import CarouselAboutTeam from '../components/CarouselAboutTeam.vue';
+import axios from 'axios';
+import { store } from '../store';
+export default {
+    name: 'Restaurants',
 
-    export default {
-        name: 'About',
+    components: {
+        TextAnimationAbout,
+        CarouselAboutTeam,
 
-        components: {
-            FadeIntoView
-        }
-
-    }
+    },
+}
 </script>
 
 <style lang="scss" scoped>
 @use './../assets/styles/partials/variables' as *;
-.demo {
-  max-width: 600px;
-  border: 2px solid #2c3e50;
-  padding: 2rem;
-  margin: 2rem auto;
-  background-color: #42b983;
+.about-container{
+    position: relative;
+    height: 100vh;
+    overflow-x: hidden;
 }
 
-.container-test{
-    height: calc(100vh - 10%);
+
+
+.blobtainer {
+    height: 100vh;
+    top: 1%;
+    right: -40%;
+    z-index: -10;
+
 }
+
+.blob {
+    width: 100%;
+    height: 100%;
+    background-size: contain;
+    background-repeat: no-repeat;
+    z-index: -1;
+    background-image: url("data:image/svg+xml;utf8,%3Csvg xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22 width=%22719.88%22 height=%22653.424%22 viewBox=%22131.714 172.967 719.88 653.424%22%3E%3Cdefs%3E%3CclipPath id=%22b%22%3E%3Cpath fill=%22currentColor%22 d=%22M823 616.5Q777 733 661 743t-244 66.5q-128 56.5-160.5-76t-99-257.5Q91 351 196 253t231-76q126 22 256 41t158 150.5q28 131.5-18 248Z%22%2F%3E%3C%2FclipPath%3E%3Cfilter id=%22a%22 x=%22-50vw%22 y=%22-50vh%22 width=%22100vw%22 height=%22100vh%22%3E%3CfeFlood flood-color=%22%23fff%22 result=%22neutral-gray%22%2F%3E%3CfeTurbulence type=%22fractalNoise%22 baseFrequency=%222.5%22 numOctaves=%22100%22 stitchTiles=%22stitch%22 result=%22noise%22%2F%3E%3CfeColorMatrix in=%22noise%22 type=%22saturate%22 values=%220%22 result=%22destaturatedNoise%22%2F%3E%3CfeComponentTransfer in=%22desaturatedNoise%22 result=%22theNoise%22%3E%3CfeFuncA type=%22table%22 tableValues=%220 0 0.15 0%22%2F%3E%3C%2FfeComponentTransfer%3E%3CfeBlend in=%22SourceGraphic%22 in2=%22theNoise%22 mode=%22soft-light%22 result=%22noisy-image%22%2F%3E%3C%2Ffilter%3E%3C%2Fdefs%3E%3Cg filter=%22url(%23a)%22 clip-path=%22url(%23b)%22%3E%3Cpath fill=%22%23F4F1BB%22 d=%22M823 616.5Q777 733 661 743t-244 66.5q-128 56.5-160.5-76t-99-257.5Q91 351 196 253t231-76q126 22 256 41t158 150.5q28 131.5-18 248Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E");
+}
+
+
+  
 
 
 </style>
