ba73b2ae7aa140b10e62758aa91e331d4b63e3a6
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2023-02-15 14:55:32 +0100

    RestStyling

--- src/pages/Restaurant.vue
+++ src/pages/Restaurant.vue
@@ -1,29 +1,48 @@
 <template>
     <section v-if="restaurant">
         <div class="container pt-5">
-            <!-- Ristorante Dettagli  -->
-            <h1>{{ restaurant.name }}</h1>
-            <div class="info d-flex">
-                <p><i class="fa-solid fa-location-dot"></i> {{ restaurant.address }} </p>
-                <p>Cellulare: {{ restaurant.phone }} </p>
-                <p>Aperti da: {{ restaurant.opening_days }} </p>
-                <p>Apre alle: {{ restaurant.opening_hours }} </p>
-                <p>Chiude alle: {{ restaurant.closing_hours}} </p>
+
+
+            <div class="d-flex justify-content-around gap-2 flex-wrap">
+                <div class="">
+                    <div class="blob-img">
+                        <img class="w-100 h-100 bg-danger"
+                            :src="restaurant.image ? `${store.imagBasePath}${restaurant.image}` : '../../public/img/logo-01.png'"
+                            alt="">
+                    </div>
+                </div>
+                <!-- Ristorante Dettagli  -->
+                <div class="col-md-6 col-sm-12">
+                    <h1 class="text-capitalize">{{ restaurant.name }}</h1>
+                    <div class=" d-flex flex-column">
+                        <p class="text-capitalize"><i class="fa-solid fa-location-dot"></i> &nbsp;{{
+                            restaurant.address
+                        }} </p>
+                        <p><i class="fa-solid fa-phone"></i> &nbsp;{{ restaurant.phone }} </p>
+                        <p><i class="fa-solid fa-calendar"></i> &nbsp;{{ restaurant.opening_days }} </p>
+                        <p><i class="fa-solid fa-clock"></i> &nbsp;{{ truncate(restaurant.opening_hours, 6) }} - {{
+                            truncate(restaurant.closing_hours, 6)
+                        }} </p>
+
+                    </div>
+                    <div class=" overflow-scroll description">
+                        <p>{{ restaurant.description }}</p>
+                    </div>
+                </div>
             </div>
-            <p>{{ restaurant.description }}</p>
 
             <!--FINE Ristorante Dettagli  -->
 
-            
+
             <div class="card border-0">
                 <div class="row g-0">
-                    <div class="col-md-4">
-                        
-                        <div class="blob-img">
-                            <img class="w-100 h-100 bg-danger" :src="restaurant.image ? `${store.imagBasePath}${restaurant.image}`: '../../public/img/logo-01.png'" alt="">
-                        </div>
-                        
-                        <!-- CARRELLO -->
+
+
+
+
+                    <!-- CARRELLO -->
+                    <!-- <div class="col-md-4">
+
                         <div class="my-cart">
                             <p class="pt-3 fw-semibold">Carrello:</p>
                             <ul>
@@ -31,40 +50,53 @@
                                     <p> <span class="fw-bold">{{ item.name }}</span> x{{ item.quantity }}</p>
                                 </li>
                             </ul>
-                            <p> <span class="fw-bold">Totale:</span>  {{ cartTotal }} &#8364;</p>
+                            <p> <span class="fw-bold">Totale:</span> {{ cartTotal }} &#8364;</p>
                             <button class="btn btn-primary" @click="clearCart()">Svuota</button>
                             <br>
                             <router-link :to="{ name: 'checkout' }">
-                              <button class="mt-3">checkout</button>
+                                <button class="mt-3">checkout</button>
                             </router-link>
                         </div>
-                        <!-- FINE CARRELLO -->
+                    </div> -->
+                    <!-- FINE CARRELLO -->
 
-                    </div>
+
 
                     <!-- MENU PIATTI -->
-                    <div class="col-md-8">
+                    <div class="">
                         <h4>Scegli i tuoi piatti</h4>
                         <div class="container pt-2 pb-4">
-                            <div class="row g-3">
-                                <div class="my-card-menu border rounded-5" v-for="(plate, index) in restaurant.plates"
-                                    :key="index" >
+                            <div class="row g-3 justify-content-around">
+                                <div class="col-md-5 col-sm-12 border rounded-5 " v-for="(plate, index) in restaurant.plates"
+                                    :key="index">
                                     <div class="card-body d-flex ">
                                         <div class="me-3">
-                                            <img :src="`${store.imagBasePath}${plate.image}`" alt="">
+                                            <img :src="plate.image ? `${store.imagBasePath}${plate.image}` : '../../public/img/boolean.png'"
+                                                alt="">
                                         </div>
-                                        <div>
+
+
+                                        <FadeIntoView>
+
+                                            <div>
                                             <p class="fw-semibold">{{ plate.name }}</p>
                                             <p>{{ plate.price }} &#8364;</p>
 
-                                            <p v-if="( plate.allergens )">Allergeni: {{ plate.allergens }} </p>
+                                            <p v-if="(plate.allergens)">Allergeni: {{ plate.allergens }} </p>
                                             <p v-else="( plate.allergens )"> Non ci sono allergeni</p>
 
                                             <!-- <p>{{ plate.available == 1 ? 'Sì è disponibile' : 'No disponibile' }}</p> -->
-                                            <button class="my-button fa-solid fa-plus fa-beat" @click="addToCart(plate)"></button>
-                                            <button class="my-button fa-solid fa-minus fa-beat" @click="removeFromCart(plate)"></button>
+                                            <button class="my-button fa-solid fa-plus "
+                                                @click="addToCart(plate)"></button>
+                                            <button class="my-button fa-solid fa-minus "
+                                                @click="removeFromCart(plate)"></button>
                                             <!-- <p>quantità: {{ plate.quantity }}</p> -->
                                         </div>
+                                       
+                                    </FadeIntoView>
+                                        
+
+
                                     </div>
                                 </div>
                             </div>
@@ -90,39 +122,55 @@
 </template>
 
 <script>
+import FadeIntoView from '../components/FadeIntoView.vue';
 import axios from 'axios';
 import { store } from '../store'
-    export default {
-        name: 'Restaurant',
+export default {
+    name: 'Restaurant',
 
-        data(){
-            return {
-                
-                store,
-                restaurant:null,
-                imagBasePath:''
-                
-            }
-        },
-        computed: {
-            cartTotal () {
-              if (!store.cart) {
+    components:{
+
+        FadeIntoView
+    },
+
+    data() {
+        return {
+
+            store,
+            restaurant: null,
+            imagBasePath: ''
+
+        }
+    },
+    computed: {
+        cartTotal() {
+            if (!store.cart) {
                 store.cart = [];
             }
-                let total = 0
-                for (let i = 0; i < store.cart.length; i++) {
-                    total += store.cart[i].price * store.cart[i].quantity
-                }
-                return total
+            let total = 0
+            for (let i = 0; i < store.cart.length; i++) {
+                total += store.cart[i].price * store.cart[i].quantity
             }
+            return total
+        }
+    },
+
+    methods: {
+
+
+
+        truncate(str, n) {
+            if (typeof str !== 'string') {
+                return '';
+            }
+            return (str.length > n) ? str.substring(0, n - 1) : str;
         },
 
-        methods: {
         getRestaurant() {
             axios.get(`${this.store.apiBaseUrl}/restaurants/${this.$route.params.slug}`).then((response) => {
                 console.log(response.data.results)
                 if (response.data.success) {
-                    
+
                     this.restaurant = response.data.results;
                 } else {
                     this.$router.push({ name: 'notfound' })
@@ -134,51 +182,51 @@ import { store } from '../store'
         getPlates() {
             axios.get(`${this.store.apiBaseUrl}/plates/${this.$route.params.slug}`).then((response) => {
                 console.log(response.data.results)
-             
-                    
-                    this.plates = response.data.results;
-              
+
+
+                this.plates = response.data.results;
+
             })
 
         },
 
-        getApi(){
+        getApi() {
             return (this.getRestaurant,
-             this.getPlates)
+                this.getPlates)
         },
-        addToCart(plate){
+        addToCart(plate) {
             // if (!store.cart) {
             //     store.cart = [];
             // }
             let plateInCart = false;
             for (let i = 0; i < store.cart.length; i++) {
                 if (store.cart[i].slug === plate.slug) {
-                store.cart[i].quantity++;
-                plateInCart = true;
-                break;
+                    store.cart[i].quantity++;
+                    plateInCart = true;
+                    break;
                 }
             }
             if (!plateInCart) {
-              for (let i = 0; i < store.cart.length; i++){
-                if(plate.restaurant_id !== store.cart[i].restaurant_id){
-                  if(confirm('Vuoi svuotare il carrello e ordinare dal nuovo ristorante?')){
-                    store.cart = [];
-                    console.log('non corrispondono');
-                  }else{
-                    return;
-                  }
-              }
-              }
+                for (let i = 0; i < store.cart.length; i++) {
+                    if (plate.restaurant_id !== store.cart[i].restaurant_id) {
+                        if (confirm('Vuoi svuotare il carrello e ordinare dal nuovo ristorante?')) {
+                            store.cart = [];
+                            console.log('non corrispondono');
+                        } else {
+                            return;
+                        }
+                    }
+                }
                 plate.quantity = 1;
                 store.cart.push(plate);
             }
             localStorage.setItem("cart", JSON.stringify(store.cart));
-            console.log("Piatto aggiunto al carrello"); 
-            console.log( store.cart); 
+            console.log("Piatto aggiunto al carrello");
+            console.log(store.cart);
         },
 
         removeFromCart(plate) {
-            if(store.cart.length>0){
+            if (store.cart.length > 0) {
                 for (let i = 0; i < store.cart.length; i++) {
                     if (store.cart[i].slug === plate.slug) {
                         store.cart[i].quantity--;
@@ -191,59 +239,65 @@ import { store } from '../store'
                 localStorage.setItem("cart", JSON.stringify(store.cart));
                 console.log("Plate rimosso dal carrello");
             }
-            console.log( store.cart); 
+            console.log(store.cart);
         },
         clearCart() {
             store.cart = [];
             localStorage.removeItem("cart");
-            console.log( store.cart); 
+            console.log(store.cart);
             console.log("Carrello svuotato");
-            },
-        
+        },
+
     },
 
     mounted() {
-      const storedCart = localStorage.getItem('cart');
+        const storedCart = localStorage.getItem('cart');
         if (storedCart) {
             store.cart = JSON.parse(storedCart);
         }
 
         setTimeout(
-            this.getRestaurant,1500,
-            this.getPlates,1500
+            this.getRestaurant, 1500,
+            this.getPlates, 1500
         )
-          
-        
+
+
 
     }
-        
 
-    }
+
+}
 </script>
 
 <style lang="scss" scoped>
 @use './../assets/styles/partials/variables' as *;
 
 // Ristorante Dettagli
-.info {
-    p:not(:first-child) {
-        margin: 0 5px;
-    }
+h1 {
+    font-size: 2.5rem;
+    font-weight: 900;
+    color: $acqua;
+}
 
-    p::after {
-        content: " \00B7 ";
-    }
+h4{
+    font-size: 2.5rem;
+    font-weight: 900;
+    color: $red;  
+}
 
-    p:last-child:after {
-        content: " ";
-    }
+i {
+    color: $red;
+}
+
+.description {
+    height: 30%;
 }
 
 // MENU CARDS
-.my-card-menu{
-	margin: 10px 10px;
-	width: calc(50% - 20px);
-	background-color: white;
+.my-card-menu {
+    margin: 10px 10px;
+    width: calc(50% - 20px);
+    background-color: white;
 }
 
 // Immagine piatto singlo
@@ -254,35 +308,36 @@ import { store } from '../store'
     border-radius: 50%;
 }
 
-@media (max-width: 1024px){
+@media (max-width: 1024px) {
     .card-body img {
         width: 80px;
         height: 80px;
     }
 }
 
-.fa-plus:hover{
+.fa-plus:hover {
     color: blue;
 }
 
-.fa-minus:hover{
+.fa-minus:hover {
     color: red;
 }
+
 .fa-solid:not(:hover) {
-   animation: none;
+    animation: none;
 }
 
 // CARELLO
-.my-cart{
+.my-cart {
     position: sticky;
     top: 0;
     z-index: 10;
 }
 
 .my-button {
-    background-color: white;
+    background-color: $red;
     border: none;
-    color: black;
+    color: $yellow;
     // padding: 10px 24px;
     text-align: center;
     text-decoration: none;
@@ -292,80 +347,89 @@ import { store } from '../store'
     transition-duration: 0.4s;
     cursor: pointer;
     border-radius: 12px;
-  }
+    transition: 0.5s ease-in-out;
+
+    &:hover{
+        transform: scale(1.2);
+        color: $yellow;
+    }
+}
 
 // Loader
 .loader {
-   height: 70vh;
+    height: 70vh;
 }
 
 .load-img {
     top: 40%;
-  left: 50%;
-  position: absolute;
-  transform: translate(-50%, -50%);
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%, -50%);
     width: 100px;
     height: 100px;
 }
 
 .progress-loader {
-  top: 50%;
-  left: 50%;
-  position: absolute;
-  transform: translate(-50%, -50%);
-  width: 200px;
-  background: rgba(236, 236, 238, 0.253);
-  height: 10px;
-  border-radius: 7px;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    width: 200px;
+    background: rgba(236, 236, 238, 0.253);
+    height: 10px;
+    border-radius: 7px;
 }
 
 .progress {
-  width: 1px;
-  height: 10px;
-  border-radius: 7px;
-  background: $red;
-  transition: 0.5s;
-  animation: loading_44 3s ease infinite;
+    width: 1px;
+    height: 10px;
+    border-radius: 7px;
+    background: $red;
+    transition: 0.5s;
+    animation: loading_44 3s ease infinite;
 }
 
 @keyframes loading_44 {
-  0% {
-    width: 0%;
-  }
+    0% {
+        width: 0%;
+    }
 
-  50% {
-    width: 100%;
-  }
+    50% {
+        width: 100%;
+    }
 
-  100% {
-    width: 0%;
-  }
+    100% {
+        width: 0%;
+    }
 }
 
 
 // image container
 
-.blob-img{
+.blob-img {
     overflow: hidden;
     width: 400px;
     height: 400px;
     border-radius: 42% 56% 72% 28% / 42% 42% 56% 48%;
-    animation: blob 10s linear infinite; ;
+    animation: blob 10s linear infinite;
+    ;
 }
 
 @keyframes blob {
-      0%, 100%{
+
+    0%,
+    100% {
         border-radius: 42% 56% 72% 28% / 42% 42% 56% 48%;
-      }  
-      33%{
+    }
+
+    33% {
         border-radius: 42% 28% 62% 18% / 28% 28% 89% 89%;
-      }
+    }
 
-      66%{
+    66% {
         border-radius: 100% 56% 56% 100% / 100% 100% 79% 79%;
-      }
+    }
 
-      
+
 }
-
 </style>
