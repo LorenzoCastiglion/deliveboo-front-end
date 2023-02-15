10228d61ae8ca26de7af7a9e840dd215ed8c99e3
MicheleBenaco <benaco.michele@yahoo.it>
2023-02-15 15:02:18 +0100

    fix-home-contaienr

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,12 @@
 <template>
-    <section class=" d-flex justify-content-center align-items-center flex-wrap pt-5">
-        <div class="position-absolute blobtainer col-12 ">
+    <section class=" d-flex justify-content-center align-items-center flex-wrap pt-5 about-container ">
+        <div class="position-absolute blobtainer col-12  ">
             <div class="blob "></div>
         </div>
+        <div class="position-absolute blobtainer3 col-12 overflow-visible ">
+                <div class="blob3 "></div>
+            </div>
+        
         <!-- SCRITTA CENTRALE -->
         <div class="container col-12 pb-5">
             <TextAnimationAbout></TextAnimationAbout>
@@ -36,7 +40,7 @@
 
         <!--  RECENSIONI -->
             <!--Testimonials------------------->
-    <section id="testimonials " class="cont-pading">
+    <section id="testimonials " class="cont-pading container">
         <!--heading--->
         <div class="testimonial-heading">
             <span>Commenti</span>
@@ -204,6 +208,9 @@ export default {
 
 <style lang="scss" scoped>
 @use './../assets/styles/partials/variables' as *;
+
+
+
 .about-container{
     position: relative;
     height: 100vh;
@@ -227,6 +234,25 @@ export default {
     background-repeat: no-repeat;
     z-index: -1;
     background-image: url("data:image/svg+xml;utf8,%3Csvg xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22 width=%22719.88%22 height=%22653.424%22 viewBox=%22131.714 172.967 719.88 653.424%22%3E%3Cdefs%3E%3CclipPath id=%22b%22%3E%3Cpath fill=%22currentColor%22 d=%22M823 616.5Q777 733 661 743t-244 66.5q-128 56.5-160.5-76t-99-257.5Q91 351 196 253t231-76q126 22 256 41t158 150.5q28 131.5-18 248Z%22%2F%3E%3C%2FclipPath%3E%3Cfilter id=%22a%22 x=%22-50vw%22 y=%22-50vh%22 width=%22100vw%22 height=%22100vh%22%3E%3CfeFlood flood-color=%22%23fff%22 result=%22neutral-gray%22%2F%3E%3CfeTurbulence type=%22fractalNoise%22 baseFrequency=%222.5%22 numOctaves=%22100%22 stitchTiles=%22stitch%22 result=%22noise%22%2F%3E%3CfeColorMatrix in=%22noise%22 type=%22saturate%22 values=%220%22 result=%22destaturatedNoise%22%2F%3E%3CfeComponentTransfer in=%22desaturatedNoise%22 result=%22theNoise%22%3E%3CfeFuncA type=%22table%22 tableValues=%220 0 0.15 0%22%2F%3E%3C%2FfeComponentTransfer%3E%3CfeBlend in=%22SourceGraphic%22 in2=%22theNoise%22 mode=%22soft-light%22 result=%22noisy-image%22%2F%3E%3C%2Ffilter%3E%3C%2Fdefs%3E%3Cg filter=%22url(%23a)%22 clip-path=%22url(%23b)%22%3E%3Cpath fill=%22%23F4F1BB%22 d=%22M823 616.5Q777 733 661 743t-244 66.5q-128 56.5-160.5-76t-99-257.5Q91 351 196 253t231-76q126 22 256 41t158 150.5q28 131.5-18 248Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E");
+}
+
+.blob3 {
+
+width: 100%;
+height: 100%;
+background-size: contain;
+background-repeat: no-repeat;
+background-image: url("data:image/svg+xml;utf8,%3Csvg xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22 width=%22739.408%22 height=%22614.268%22 viewBox=%22187.485 208.685 739.408 614.268%22%3E%3Cdefs%3E%3CclipPath id=%22b%22%3E%3Cpath fill=%22currentColor%22 d=%22M884 682q-94 182-268.5 129t-309-103q-134.5-50-117-199.5t135.5-205q118-55.5 273.5-89t267.5 126Q978 500 884 682Z%22%2F%3E%3C%2FclipPath%3E%3Cfilter id=%22a%22 x=%22-50vw%22 y=%22-50vh%22 width=%22100vw%22 height=%22100vh%22%3E%3CfeFlood flood-color=%22%23fff%22 result=%22neutral-gray%22%2F%3E%3CfeTurbulence type=%22fractalNoise%22 baseFrequency=%222.5%22 numOctaves=%22100%22 stitchTiles=%22stitch%22 result=%22noise%22%2F%3E%3CfeColorMatrix in=%22noise%22 type=%22saturate%22 values=%220%22 result=%22destaturatedNoise%22%2F%3E%3CfeComponentTransfer in=%22desaturatedNoise%22 result=%22theNoise%22%3E%3CfeFuncA type=%22table%22 tableValues=%220 0 0.35 0%22%2F%3E%3C%2FfeComponentTransfer%3E%3CfeBlend in=%22SourceGraphic%22 in2=%22theNoise%22 mode=%22soft-light%22 result=%22noisy-image%22%2F%3E%3C%2Ffilter%3E%3C%2Fdefs%3E%3Cg filter=%22url(%23a)%22 clip-path=%22url(%23b)%22%3E%3Cpath fill=%22%23ed6a5a%22 d=%22M884 682q-94 182-268.5 129t-309-103q-134.5-50-117-199.5t135.5-205q118-55.5 273.5-89t267.5 126Q978 500 884 682Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E");
+
+}
+
+.blobtainer3 {
+    height: 100vh;
+    top: 80%;
+  left: -10%;
+    z-index: -15;
+    // transform: rotate(140deg);
+
 }
 
 .cont-pading{
